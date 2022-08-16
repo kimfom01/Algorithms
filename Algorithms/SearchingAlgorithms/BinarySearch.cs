4e2723fdf80ac3cd25f40f73031480bc8a8c92af
Kim <kimfom01@gmail.com>
2022-08-16 19:12:27 +0300

    Add project files.

---
+++ Algorithms/SearchingAlgorithms/BinarySearch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.SearchingAlgorithms
+{
+    public class BinarySearch
+    {
+        public BinarySearch()
+        {
+
+        }
+
+        /// <summary>
+        /// Method to search for a given key from an array and specifying whether ascending or descending
+        /// </summary>
+        /// <param name="Arr">Array to search from</param>
+        /// <param name="key">key to look for</param>
+        /// <returns>the index of the array if found else -1</returns>
+        public double Search(double[] Arr, double key)
+        {
+            int left, right, middle, key_pos;
+            bool found;
+
+            left = 0;
+            right = Arr.Length - 1;
+            found = false;
+            key_pos = -1;
+            while (left <= right && found == false)
+            {
+                middle = (int)((left + right) / 2);
+
+                if (key < Arr[middle])
+                {
+                    right = middle - 1;
+                }
+                else if (key > Arr[middle])
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    found = true;
+                    key_pos = middle;
+                }
+            }
+
+            return key_pos;
+        }
+    }
+}
---
+++ Algorithms/SearchingAlgorithms/LinearSearch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.SearchingAlgorithms
+{
+    public class LinearSearch
+    {
+        public LinearSearch()
+        {
+
+        }
+
+        /// <summary>
+        /// Method to search for a given key from an array and specifying whether ascending or descending
+        /// </summary>
+        /// <param name="Arr">Array to search from</param>
+        /// <param name="key">key to look for</param>
+        /// <returns>the index of the array if found else -1</returns>
+        public double Search(double[] Arr, double key)
+        {
+            int i, size;
+
+            i = 0;
+            size = Arr.Length;
+            while (i <= size - 1 && Arr[i] != key)
+            {
+                i++;
+            }
+
+            if (Arr[i] == key)
+            {
+                return i;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
