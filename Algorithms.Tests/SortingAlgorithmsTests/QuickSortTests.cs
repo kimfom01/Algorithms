3af4c4201567cd76ce1c1804ff45efc6ad2c36ce
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-11-16 14:58:54 +0300

    Change the test values

--- Algorithms.Tests/SortingAlgorithmsTests/QuickSortTests.cs
+++ Algorithms.Tests/SortingAlgorithmsTests/QuickSortTests.cs
@@ -6,16 +6,16 @@ namespace Algorithms.Tests.SortingAlgorithmsTests
     public class QuickSortTests
     {
         [Test]
-        public void Sort_AscendingOrder([Random(0, 100, 10, Distinct = true)] int n)
-        { // Failing test need to refactor code
-            var (actual, expected) = GetArrays(n);
-
+        public void Sort_AscendingOrder()
+        {
+            int[] numbers = { 3, 2, 6, 9, 7, 5, 4, 8, 1 };
+            int[] sortedNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            
             int start = 0;
-            int end = actual.Length;
-            QuickSort<int>.Sort(actual, start, end);
-            Array.Sort(expected);
+            int end = numbers.Length;
+            QuickSort<int>.Sort(numbers, start, end);
 
-            Assert.AreEqual(actual, expected);
+            Assert.That(numbers, Is.EqualTo(sortedNumbers));
         }
         
         [Test]
@@ -30,21 +30,6 @@ namespace Algorithms.Tests.SortingAlgorithmsTests
             QuickSort<int>.Sort(numbers, start, end, false);
 
             Assert.That(numbers, Is.EqualTo(sortedNumbers));
-        }
-        
-        private (int[] actual, int[] expected) GetArrays(int n)
-        {
-            var actual = new int[n];
-            var expectedArray = new int[n];
-
-            for (int i = 0; i <= n - 1; i++)
-            {
-                int randNum = Random.Shared.Next(100);
-                actual[i] = randNum;
-                expectedArray[i] = randNum;
-            }
-
-            return (actual, expectedArray);
         }
     }
 }
